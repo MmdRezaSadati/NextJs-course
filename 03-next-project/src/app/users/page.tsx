1eb09b353cf97870ce08853d0d6a79a6278ba309
MmdRezaSadati <sadaty05@gmail.com>
2025-09-02 20:20:02 +0330

    add container for session 5 video 01 (styling with tailwindcss)

--- 03-next-project/src/app/users/page.tsx
+++ 03-next-project/src/app/users/page.tsx
@@ -1,10 +1,11 @@
+import Container from "@/components/container/Container";
 import { getUsers } from "@/core/api/users";
 
 const UsersPage = async () => {
   const users = await getUsers();
   console.log("users", users);
   return (
-    <div>
+    <Container>
       <h1>Our users</h1>
       <ul className="flex flex-wrap gap-2">
         {users.map((value) => (
@@ -14,7 +15,7 @@ const UsersPage = async () => {
           </div>
         ))}
       </ul>
-    </div>
+    </Container>
   );
 };
 
